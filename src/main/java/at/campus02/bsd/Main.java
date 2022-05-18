b62b86b61a391713789747e64c22265e32d594b2
Janzi14 <jakobjanz1@gmail.com>
2022-05-18 10:44:08 +0200

    updated main

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -2,17 +2,23 @@
  * Main
  * A simple calculator
  * Author: Jakob Janz
- * Last Change: 06.04.2022
+ * Last Change: 18.05.2022
  */
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
+        logger.info("This is an informative message");
         Calculator calculator =new Calculator();
         System.out.println(calculator.add(2.44,5.78));
         System.out.println(calculator.minus(2.44,5.78));
         System.out.println( calculator.multiply(2.44,5.78));
         System.out.println(  calculator.divide(2.44,5.78));
+        logger.error("This is an error message");
 
 
 
