85412eff7b5628a96511d896e4db909b5f3f3d1e
Jakob Janz <s50919@edu.campus02.at>
2022-05-25 08:55:50 +0200

    Add Javadocs to Main.java
    In this commit Javadocs are implemented in the Main.java

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,8 +1,10 @@
-/*
+/**
  * Main
- * A simple calculator
- * Author: Jakob Janz
- * Last Change: 18.05.2022
+ *
+ * The Main class prints the results of some calculations of the @Calculator.java.
+ *
+ * @author Jakob Janz
+ *
  */
 package at.campus02.bsd;
 
@@ -10,7 +12,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
+    /**
+     * The logger variable implements the logger used to log an info and an error.
+     */
     private static final Logger logger = LogManager.getLogger();
+    /**
+     * <p>The main() is the starting point of the program.
+     * </p>
+     * @param args is not used for this programm
+     */
     public static void main(String[] args) {
         logger.info("This is an informative message");
         Calculator calculator =new Calculator();
